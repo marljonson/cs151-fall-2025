96147e5cfe51087af142d2c4af64df9faf88b060
Myo Thant Zin <your_github_myothantzin.mtz412@gmail.com>
2025-09-17 03:50:25 -0700

    Testing

--- CS151Project1/src/Customers.java
+++ CS151Project1/src/Customers.java
@@ -4,4 +4,13 @@ public class Customers {
 	private String lastName;
 	private int userId;
 	
+	private int numOfUsers = 0;
+	
+	
+	public Customers(String firstName, String lastName, int userId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userId = userId;
+		this.numOfUsers++;
+	}
 }
