9f0af996c3af9243e2715b3285531155e1a579f5
myotz <myothantzin.mtz412@gmail.com>
2025-09-23 13:34:33 -0700

    Update Rentable.java sync with main

--- CS151Project1/src/Rentable.java
+++ CS151Project1/src/Rentable.java
@@ -1,1 +1,11 @@
 
+public interface Rentable {
+    //boolean isRentable();
+
+    //double getRentalPrice();
+
+    //void rentalReturn();
+
+    void rent(int quantity); //added the argument
+    void reserve(int quantity);
+}
