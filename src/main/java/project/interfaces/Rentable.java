1b6280ad53fb275b5eb2edf848554b0653cd904e
Miyuki Tokuhara <150103565+iyuki34@users.noreply.github.com>
2025-10-07 22:21:31 -0700

    Updated Rentable.java and DigiCam.java based on the temporary files

--- src/main/java/project/interfaces/Rentable.java
+++ src/main/java/project/interfaces/Rentable.java
@@ -1,13 +1,20 @@
 package interfaces;
 
+import java.time.Instant;
+
 public interface Rentable {
     boolean isRentable();
 
-    double getRentalPrice();
+    double quoteRental(Instant now); 
+    /* pure review
+     * MUST use Product's getEffectiveUnitPrice at now
+     */
+    double rent(Instant quotedAt, double expectedTotal);
+    /*
+     * give the customer an option to rent after quoted
+     * if the customer chooses rent, rent the product with the quotedAt price (the price the user saw)
+     * set stock = 0, return the charged price (s.t we can subtract that from customer's balance and add it to the vendor)
+     */
 
-    void rentalReturn();
-
-    void rent();
-
-    void rent(int amount);
+    void rentalReturn(Instant returnedAt); //might not need returnedAt if we won't charge late fees but looks really good to have this
 }
