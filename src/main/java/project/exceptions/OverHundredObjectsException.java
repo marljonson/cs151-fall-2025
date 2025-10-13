47552cd2717a97c7736276bcf4fc5753f28c1f6d
Marl Jonson <marl.jonson@comcast.net>
2025-10-12 23:59:44 -0700

    Update OverHundredObjectsException.java

--- src/main/java/project/exceptions/OverHundredObjectsException.java
+++ src/main/java/project/exceptions/OverHundredObjectsException.java
@@ -1,13 +1,21 @@
-package exceptions;
+package project.exceptions;
 
-public class OverHundredObjectsException extends Exception {
-    
-    public OverHundredObjectsException() {
-        System.out.println("Cannot create more than 100 objects of this class.");
+public class OverHundredObjects extends Exception {
+
+    public OverHundredObjects() {
+        super();
+        //System.out.println("Cannot create more than 100 objects of this class."); //will use this error when we catch this, not here
     }
 
-    public OverHundredObjectsException(String message) {
+    public OverHundredObjects(String message){
         super(message);
     }
+
+    public OverHundredObjects(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public OverHundredObjects(Throwable cause){
+        super(cause);
+    }
 }
- 
